0f1a994d170ff875192eb38165f6332e5e308410
13910n <alexandr.lobashov@mail.ru>
2024-11-04 00:45:44 +0100

    Create logger + config

---
+++ cmd/qr_code_scanner/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"qr_code_scanner/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting logger", slog.String("env", cfg.Env))
+	log.Debug("Debug info")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
